2a3cc1e54da013021cbc7270e7215af7fbdc1dea
Siddak-cloud <85190641+Siddak-cloud@users.noreply.github.com>
2021-08-06 13:51:12 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCsr6K9Q0jInkXJarz2110MC8QmM_JdVEQ",
+    authDomain: "siddak-a7b94.firebaseapp.com",
+    databaseURL: "https://siddak-a7b94-default-rtdb.firebaseio.com",
+    projectId: "siddak-a7b94",
+    storageBucket: "siddak-a7b94.appspot.com",
+    messagingSenderId: "639813716156",
+    appId: "1:639813716156:web:96529cf3c7bda2d35e68b9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
